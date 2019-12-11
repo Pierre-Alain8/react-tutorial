d1d31566b6ec3f55e2cf2e186548fe4809508723
root <root@DESKTOP-ITTSINM.localdomain>
2019-12-11 15:38:25 +0100

    Updates

---
+++ precache-manifest.a8ef7f88d72d1ad39a535b6f4ff9920e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82366870b1b009b2e93edda7641ca5d0",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "9faecd5a0c427346ccfc",
+    "url": "/react-tutorial/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "1ab8a11d26d36a982dbe",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9faecd5a0c427346ccfc",
+    "url": "/react-tutorial/static/js/main.61bb2636.chunk.js"
+  },
+  {
+    "revision": "b9cf8d7406673e674e0b",
+    "url": "/react-tutorial/static/js/runtime-main.af30ece7.js"
+  }
+]);
